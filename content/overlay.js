9b619ac9d80687bba85ba494c38d5699649ba790
yellowBirdy <mieszkomieszko@gmail.com>
2015-06-29 17:49:36 +0200

    Added url and title getter

--- content/overlay.js
+++ content/overlay.js
@@ -1,11 +1,24 @@
 var DigitalHalo =  {
-    onLoad: function()  {
-        this.initialized = true;
+    init: function()  {
+        gBrowser.addEventListener("DOMContentLoaded", this.getData, false);    
     },
 
     onMenuItemCommand: function()  {
-        window.open("chrome://helloworld/content/hello.xul", "", "chrome");		      
+        window.open("chrome://digital-halo/content/hello.xul", "", "chrome");		      
+    },
+
+    getData: function(anEvent)   {
+        var doc = anEvent.originalTarget; 
+	var adr = doc.location.href;
+        var tit = doc.getElementsByTagName("title")[0].innerHTML;
+	alert(adr+ "\n"+ tit);
     }
 };
 
-window.addEventListener("load", function(e) { DigitalHalo.onLoad(e);  }, false);
+window.addEventListener("load", function load(event) { 
+    window.removeEventListener("load", load, false);    
+    DigitalHalo.init(event);  
+}, false);
+// window.addEventListener("load", function(e)  { alert("a");  }, false)
+// window.onload = alert{"a");
+// window.onload = DigitalHalo.getTitle;
